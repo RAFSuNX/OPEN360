8e0810f26e925c0103ac5534ae7ffeef72af2b78
RAFSuNX <git@rafsunx.com>
2026-04-30 11:20:34 +0600

    fix: improve gitignore coverage, split jest environments, add test script

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,22 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {}],
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  projects: [
+    {
+      displayName: 'node',
+      testEnvironment: 'node',
+      transform: { '^.+\\.tsx?$': ['ts-jest', {}] },
+      moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+      testMatch: ['**/__tests__/**/*.test.ts'],
+    },
+    {
+      displayName: 'jsdom',
+      testEnvironment: 'jsdom',
+      transform: { '^.+\\.tsx?$': ['ts-jest', {}] },
+      moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+      testMatch: ['**/__tests__/**/*.test.tsx'],
+    },
+  ],
 }
 
 export default config
